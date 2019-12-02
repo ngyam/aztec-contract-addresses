ed1df8a8175f6856c4b3176339e13e07a40ed10c
ngyam <ngy.am.zs@gmail.com>
2019-12-02 13:19:33 +0100

    fix(volta): patches removed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = require("@aztec/contract-addresses")
+const ca = require("@aztec/contract-addresses")
+const voltaAddresses = require(__dirname + '/volta.json')
+
+const getContractAddressesForNetwork = (networkId) => {
+    if (parseInt(networkId, 10) == 73799) {
+        return voltaAddresses
+    }
+    return ca.getContractAddressesForNetwork(networkId)
+}
+
+module.exports = {
+    "getContractAddressesForNetwork": getContractAddressesForNetwork,
+    "NetworkId": {
+        ...ca.NetworkId,
+        "Volta": '73799'
+    }
+}
